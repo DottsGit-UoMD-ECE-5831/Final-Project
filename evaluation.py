93f5d70f6d43b5a1cd107c2775f3418c6674e614
mattkwan16 <mattkwan16@users.noreply.github.com>
2024-08-20 14:06:10 -0400

    Add sklearn implementation and cross validation

---
+++ evaluation.py
@@ -0,0 +1,31 @@
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+class Evaluation:
+    def __init__(self):
+        pass
+
+    # y_test: output format of sklearn's train_test_split()
+    # y_pred: output format of sklearn's LogisticRegression predict()
+    def evaluate(self, y_test, y_pred):
+        # Evaluate the model's performance
+        accuracy = accuracy_score(y_test, y_pred)
+        conf_matrix = confusion_matrix(y_test, y_pred)
+        class_report = classification_report(y_test, y_pred)
+        
+        # Print the results (incl accuracy, precision, and recall)
+        print("Test Set Accuracy:", accuracy)
+        print("\nConfusion Matrix:\n", conf_matrix)
+        print("\nClassification Report:\n", class_report)
+        
+    def kfold_cross_validate(self, X, y, classifier, num_folds=12, random_state=321):
+        # Cross-validation
+        # Code snippet derived from: 
+        # https://www.geeksforgeeks.org/cross-validation-machine-learning/
+        kf = KFold(n_splits=num_folds, shuffle=True, random_state=random_state)
+        cross_val_results = cross_val_score(classifier, X, y, cv=kf)
+        # Format each element to 3 decimal places
+        xval_mean = cross_val_results.mean()
+        cross_val_results_rounded = [f"{x:.3f}" for x in cross_val_results]
+        print(f'KFold Cross-Validation Results (Accuracy): {cross_val_results_rounded}')
+        print(f'Mean Accuracy: {xval_mean:.3f}')
